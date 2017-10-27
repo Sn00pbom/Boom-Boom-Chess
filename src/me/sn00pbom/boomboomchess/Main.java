1ba9f9b8de107b9bd0554957726bd435da172892
Zeke <averagegooch@gmail.com>
2017-10-26 22:32:31 -0600

    asdf

--- src/me/sn00pbom/boomboomchess/Main.java
+++ src/me/sn00pbom/boomboomchess/Main.java
@@ -1,11 +1,15 @@
 package me.sn00pbom.boomboomchess;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
 
+    public static WebDriver webDriver;
     public static void main(String[] args) {
 
-        //dick
-        System.out.println("Dick");
-        
+        //System.setProperty("webdriver.chrome.driver", "/home/zach/Documents/chromedriver");
+        webDriver = new ChromeDriver();
+        webDriver.get("http://www.amazon.com/");
     }
 }
