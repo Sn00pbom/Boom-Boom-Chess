f7a4401346a90b6cc24bc7697c09efb3e7e8c04d
Zeke <averagegooch@gmail.com>
2017-10-26 22:42:59 -0600

    lol

--- src/me/sn00pbom/boomboomchess/Main.java
+++ src/me/sn00pbom/boomboomchess/Main.java
@@ -6,10 +6,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Main {
 
     public static WebDriver webDriver;
+
     public static void main(String[] args) {
 
-        //System.setProperty("webdriver.chrome.driver", "/home/zach/Documents/chromedriver");
+//        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        
+
         webDriver = new ChromeDriver();
         webDriver.get("http://www.amazon.com/");
     }
+
 }
+
